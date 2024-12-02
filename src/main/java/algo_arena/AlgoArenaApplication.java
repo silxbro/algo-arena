063e3644dc918112183054ca38f192115874308f
ehpark <silbroeh@gmail.com>
2024-12-02 20:11:04 +0900

    initial commit

---
+++ src/main/java/algo_arena/AlgoArenaApplication.java
@@ -0,0 +1,13 @@
+package algo_arena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoArenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgoArenaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/algo_arena/AlgoArenaApplicationTests.java
@@ -0,0 +1,13 @@
+package algo_arena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoArenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
