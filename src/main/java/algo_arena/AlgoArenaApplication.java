731fac12e9e1da4e5222ed3ec1b1e47fb5e2acc3
silxbro <silbroeh@gmail.com>
2024-12-17 23:05:23 +0900

    feat: auditing 기능 적용

--- src/main/java/algo_arena/AlgoArenaApplication.java
+++ src/main/java/algo_arena/AlgoArenaApplication.java
@@ -2,12 +2,22 @@ package algo_arena;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AlgoArenaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlgoArenaApplication.class, args);
+	}
+
+	@Bean
+	public AuditorAware<String> auditorProvider() {
+		//TODO: 세션정보 or 스프링 시큐리티 로그인 정보에서 id 받기
+		return null;
 	}
 
 }
---
+++ src/main/java/algo_arena/common/entity/BaseEntity.java
@@ -0,0 +1,23 @@
+package algo_arena.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public class BaseEntity extends BaseTimeEntity {
+
+    @CreatedBy
+    @Column(name = "created_by", updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    @Column(name = "updated_by")
+    private String lastModifiedBy;
+}
