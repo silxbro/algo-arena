dcbffda79645d47e6e94b602aa90ac7f9010a1cc
silxbro <silbroeh@gmail.com>
2024-12-17 23:05:23 +0900

    feat: auditing 기능 적용

---
+++ src/main/java/algo_arena/common/entity/BaseTimeEntity.java
@@ -0,0 +1,23 @@
+package algo_arena.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public class BaseTimeEntity {
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime lastModifiedDate;
+}
