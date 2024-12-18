fe9f86f9e8eaa513a0a64c98fefff6af02195efe
silxbro <silbroeh@gmail.com>
2024-12-18 19:54:05 +0900

    feat: 회원(Member) 도메인 service 구현

---
+++ src/main/java/algo_arena/member/service/MemberService.java
@@ -0,0 +1,47 @@
+package algo_arena.member.service;
+
+import algo_arena.member.entity.Member;
+import algo_arena.member.repository.MemberRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public Member create(Member member) {
+        memberRepository.save(member);
+        return member;
+    }
+
+    public List<Member> findAllByNickname(String nickname) {
+        return memberRepository.findByNickname(nickname);
+    }
+
+    public Member findOneById(Long id) {
+        return memberRepository.findById(id).orElseThrow();
+    }
+
+    @Transactional
+    public void updateImage(Long id, String imgUrl) {
+        Member member = memberRepository.findById(id).orElseThrow();
+        member.changeImage(imgUrl);
+    }
+
+    @Transactional
+    public void changePassword(Long id, String password) {
+        Member member = memberRepository.findById(id).orElseThrow();
+        member.changePassword(password);
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        memberRepository.deleteById(id);
+    }
+}
