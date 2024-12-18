f1e286d143c2a69606b4e6cecde659b6b6356af7
silxbro <silbroeh@gmail.com>
2024-12-18 19:54:05 +0900

    feat: 회원(Member) 도메인 service 구현

--- src/main/java/algo_arena/member/entity/Member.java
+++ src/main/java/algo_arena/member/entity/Member.java
@@ -9,6 +9,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -36,5 +37,22 @@ public class Member extends BaseEntity {
 
     private int testCount; //참가 테스트 개수
     private int problemCount; //정답 문제 개수
+
+    @Builder
+    public Member(String email, String password, String nickname, String imgUrl, Role role) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.imgUrl = imgUrl;
+        this.role = Role.USER;
+    }
+
+    public void changePassword(String password) {
+        this.password = password;
+    }
+
+    public void changeImage(String imageUrl) {
+        this.imgUrl = imageUrl;
+    }
 
 }
