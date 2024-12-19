988b2c54a1f1d05493cd25b036d0e726ba1dfd80
silxbro <silbroeh@gmail.com>
2024-12-19 16:52:18 +0900

    feat: 문제(Problem) 도메인 entity 수정
    - 문제번호 필드 추가
    - builder 생성자 생성

--- src/main/java/algo_arena/problem/entity/Problem.java
+++ src/main/java/algo_arena/problem/entity/Problem.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -20,7 +21,14 @@ public class Problem extends BaseEntity {
     @Column(name = "problem_id")
     private Long id;
 
+    private Integer number;
     private String title;
     private String link;
 
+    @Builder
+    public Problem(Integer number, String title, String link) {
+        this.number = number;
+        this.title = title;
+        this.link = link;
+    }
 }
