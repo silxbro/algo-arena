12ce9066031569b96d9ee59749d6d2f74dc77c44
silxbro <silbroeh@gmail.com>
2024-12-20 15:14:28 +0900

    feat: 제출(Submission) 검색 조건 dto 생성자 builder 추가

--- src/main/java/algo_arena/submission/dto/request/SubmissionSearchCond.java
+++ src/main/java/algo_arena/submission/dto/request/SubmissionSearchCond.java
@@ -1,5 +1,6 @@
 package algo_arena.submission.dto.request;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
@@ -10,4 +11,13 @@ public class SubmissionSearchCond {
     private String languageName; //풀이 언어 이름
     private String resultDescription; //결과 코드
 
+    @Builder
+
+    public SubmissionSearchCond(Integer problemNumber, String memberNickname, String languageName,
+        String resultDescription) {
+        this.problemNumber = problemNumber;
+        this.memberNickname = memberNickname;
+        this.languageName = languageName;
+        this.resultDescription = resultDescription;
+    }
 }
