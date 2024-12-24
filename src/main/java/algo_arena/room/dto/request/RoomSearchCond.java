bab60a1b593705c16f53923c57fbd9be9a838ac2
silxbro <silbroeh@gmail.com>
2024-12-24 18:21:17 +0900

    feat: 테스트방(Room) 도메인 repository 구현
    - 상세조건 검색 기능 구현: 검색 Predicate(조건) 모듈화

---
+++ src/main/java/algo_arena/room/dto/request/RoomSearchCond.java
@@ -0,0 +1,18 @@
+package algo_arena.room.dto.request;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class RoomSearchCond {
+
+    private String roomName;
+    private Integer maxParticipants;
+    private String languageName;
+    private Integer minProblems;
+    private Integer maxProblems;
+    private Integer minTimeLimit;
+    private Integer maxTimeLimit;
+
+}
