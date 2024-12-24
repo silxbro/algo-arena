531663a028b7068834107eb87dab1fc44c538e4d
silxbro <silbroeh@gmail.com>
2024-12-24 18:03:47 +0900

    feat: 테스트방(Room) 도메인 Entity 및 참가자(Participant) 타입 생성
    - 참가자 타입은 id + 준비여부(ready) 정보 포함
    - id는 UUID를 사용하여 유일성 보장, id로 객체 식별
    - timeToLive 필드는 redis 데이터 생존 시간 설정(초 단위)

---
+++ src/main/java/algo_arena/room/entity/Room.java
@@ -0,0 +1,50 @@
+package algo_arena.room.entity;
+
+import algo_arena.submission.entity.Language;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+
+@Getter
+@RedisHash("room")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Room implements Serializable {
+
+    @Id
+    @EqualsAndHashCode.Include
+    private String id = UUID.randomUUID().toString();
+
+    private String name;
+    private Integer maxParticipants;
+    private List<Long> problemIds;
+    private Long hostId;
+    private List<Participant> participants = new ArrayList<>();
+
+    @Enumerated(EnumType.STRING)
+    private Language language;
+
+    private Integer timeLimit; //분 단위
+
+    @TimeToLive
+    private Long ttl;
+
+    @Builder
+    public Room(String name, Integer maxParticipants, List<Long> problemIds, Long hostId, Language language, Integer timeLimit, Long ttl) {
+        this.name = name;
+        this.maxParticipants = maxParticipants;
+        this.problemIds = problemIds;
+        this.hostId = hostId;
+        this.language = language;
+        this.timeLimit = timeLimit;
+        this.ttl = ttl;
+    }
+}
