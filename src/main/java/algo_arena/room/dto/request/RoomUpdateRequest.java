0f636ae057080d5a57542698dfade941986b25f3
silxbro <silbroeh@gmail.com>
2024-12-26 11:30:05 +0900

    feat: 테스트방(Room) 도메인 수정(변경) 기능 구현
    - 테스트방 정보 수정(방장 권한) 기능
    - 테스트방 입장/퇴장 기능
    - 관련 dto 생성

---
+++ src/main/java/algo_arena/room/dto/request/RoomUpdateRequest.java
@@ -0,0 +1,26 @@
+package algo_arena.room.dto.request;
+
+import algo_arena.room.entity.Room;
+import algo_arena.submission.entity.Language;
+import java.util.List;
+import lombok.Data;
+
+@Data
+public class RoomUpdateRequest {
+
+    private String name;
+    private Integer maxEntrants;
+    private List<Long> problemIds;
+    private String languageName;
+    private Integer timeLimit;
+
+    public Room toEntity() {
+        return Room.builder()
+            .name(name)
+            .maxEntrants(maxEntrants)
+            .problemIds(problemIds)
+            .language(Language.fromName(languageName))
+            .timeLimit(timeLimit)
+            .build();
+    }
+}
