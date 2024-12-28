81dd61242cc2638e64973e7a0606e58c6438c8d4
silxbro <silbroeh@gmail.com>
2024-12-28 16:46:52 +0900

    feat: Embedded Redis(in-memory redis) 테스트환경 설정

---
+++ src/main/java/algo_arena/config/redis/EmbeddedRedisConfig.java
@@ -0,0 +1,32 @@
+package algo_arena.config.redis;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import redis.embedded.RedisServer;
+
+@Configuration
+@Profile("test")
+public class EmbeddedRedisConfig {
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    private RedisServer redisServer;
+
+    @PostConstruct
+    public void startRedis() throws IOException {
+        redisServer = new RedisServer(redisPort);
+        redisServer.start();
+    }
+
+    @PreDestroy
+    public void stopRedis() {
+        if (redisServer != null) {
+            redisServer.stop();
+        }
+    }
+}
