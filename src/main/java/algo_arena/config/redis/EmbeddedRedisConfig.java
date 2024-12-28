1d36326efc5609bbf2db5b87ad807da354ee2b46
silxbro <silbroeh@gmail.com>
2024-12-28 16:54:09 +0900

    feat: embedded redis 사용 취소
    - embedded redis 오작동으로 인한 테스트환경 제외

--- src/main/java/algo_arena/config/redis/EmbeddedRedisConfig.java
+++ src/main/java/algo_arena/config/redis/EmbeddedRedisConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import redis.embedded.RedisServer;
 
-@Configuration
+//@Configuration
 @Profile("test")
 public class EmbeddedRedisConfig {
 
