65649d6afa94afa4acbf32cd4b626138c0f4a4db
silxbro <silbroeh@gmail.com>
2025-01-03 19:06:14 +0900

    feat: 테스트룸(Room) Redis Repository - dto 저장 및 반환하도록 수정

--- src/main/java/algo_arena/room/repository/RoomRedisRepository.java
+++ src/main/java/algo_arena/room/repository/RoomRedisRepository.java
@@ -1,14 +1,10 @@
 package algo_arena.room.repository;
 
-import algo_arena.room.dto.request.RoomSearchRequest;
+import algo_arena.room.dto.response.RedisRoom;
 import algo_arena.room.entity.Room;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.annotation.PostConstruct;
-import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.TimeUnit;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
@@ -28,22 +24,16 @@ public class RoomRedisRepository {
         opsValueOperation = redisTemplate.opsForValue();
     }
 
-    public Room save(Room room) {
+    public void save(Room room) {
         String key = generateKey(room.getId());
-        opsValueOperation.set(key, room, 24, TimeUnit.HOURS);
-        return room;
+        RedisRoom redisRoom = RedisRoom.from(room);
+        opsValueOperation.set(key, redisRoom);
     }
 
-    public List<Room> findAllBySearch(RoomSearchRequest request) {
-        return redisTemplate.keys(ROOM_KEY_PREFIX + "*").stream()
-            .map(this::getRoomFromCache)
-            .filter(matches(request))
-            .collect(Collectors.toList());
-    }
 
     public Optional<Room> findById(String id) {
         String key = generateKey(id);
-        return Optional.ofNullable(getRoomFromCache(key));
+        return Optional.ofNullable(RedisRoom.toRoom(getRoomFromCache(key)));
     }
 
     public void deleteById(String id) {
@@ -55,39 +45,9 @@ public class RoomRedisRepository {
         redisTemplate.delete(redisTemplate.keys(ROOM_KEY_PREFIX + "*"));
     }
 
-    private Room getRoomFromCache(String key) {
+    private RedisRoom getRoomFromCache(String key) {
         Object roomObject = opsValueOperation.get(key);
-        return objectMapper.convertValue(roomObject, Room.class);
-    }
-
-    private Predicate<Room> matches(RoomSearchRequest request) {
-        return roomNameContains(request.getRoomName())
-            .and(maxRoomMembersEq(request.getMaxRoomMembers()))
-            .and(languageEq(request.getLanguageName()))
-            .and(problemsBetween(request.getMinProblems(), request.getMaxProblems()))
-            .and(timeLimitBetween(request.getMinTimeLimit(), request.getMaxTimeLimit()));
-    }
-
-    private Predicate<Room> roomNameContains(String roomName) {
-        return room -> roomName == null || room.getName().toUpperCase().contains(roomName.toUpperCase());
-    }
-
-    private Predicate<Room> maxRoomMembersEq(Integer maxRoomMembers) {
-        return room -> maxRoomMembers == null || room.getMaxRoomMembers() <= maxRoomMembers;
-    }
-
-    private Predicate<Room> languageEq(String languageName) {
-        return room -> languageName == null || room.getLanguage().getName().equals(languageName);
-    }
-
-    private Predicate<Room> problemsBetween(Integer minProblems, Integer maxProblems) {
-        return room -> (minProblems == null || room.getRoomProblems().size() >= minProblems) &&
-            (maxProblems == null || room.getRoomProblems().size() <= maxProblems);
-    }
-
-    private Predicate<Room> timeLimitBetween(Integer minTimeLimit, Integer maxTimeLimit) {
-        return room -> (minTimeLimit == null || room.getTimeLimit() >= minTimeLimit) &&
-            (maxTimeLimit == null || room.getTimeLimit() <= maxTimeLimit);
+        return objectMapper.convertValue(roomObject, RedisRoom.class);
     }
 
     private String generateKey(String roomId) {
