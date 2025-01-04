dee854c5205692c78b0c4cd69ed72fc3494df0c5
silxbro <silbroeh@gmail.com>
2025-01-04 12:27:52 +0900

    chore: WebSocket 의존 주입 및 설정

---
+++ src/main/java/algo_arena/config/socket/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package algo_arena.config.socket;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/sub"); // 메시지를 브로드캐스팅할 경로
+        registry.setApplicationDestinationPrefixes("/pub"); // 클라이언트 요청 수신 경로
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS(); // STOMP 엔드포인트 등록
+    }
+}
