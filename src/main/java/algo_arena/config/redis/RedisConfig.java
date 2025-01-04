bec1aa138539895d0f0059e6908bb57fc57cce24
silxbro <silbroeh@gmail.com>
2025-01-04 11:53:10 +0900

    feat: ChannelTopic Bean 생성

--- src/main/java/algo_arena/config/redis/RedisConfig.java
+++ src/main/java/algo_arena/config/redis/RedisConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -21,6 +22,11 @@ public class RedisConfig {
 
     @Value("${spring.data.redis.port}")
     private int port;
+
+    @Bean
+    public ChannelTopic channelTopic() {
+        return new ChannelTopic("test-room");
+    }
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
