7caa3808c3e9feb29e0b6dacd0d0e98caf0e8f95
silxbro <silbroeh@gmail.com>
2025-01-04 14:34:06 +0900

    feat: 채팅메시지(ChatMessage) controller 구현
    - 채팅메시지 전송 기능

---
+++ src/main/java/algo_arena/chat/controller/ChatMessageController.java
@@ -0,0 +1,43 @@
+package algo_arena.chat.controller;
+
+import algo_arena.chat.dto.ClientMessage;
+import algo_arena.chat.dto.request.ChatSendRequest;
+import algo_arena.chat.enums.ClientMessageType;
+import algo_arena.chat.factory.SocketMessageFactory;
+import algo_arena.chat.service.ChattingService;
+import algo_arena.member.entity.Member;
+import algo_arena.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class ChatMessageController {
+
+    private final ChattingService chattingService;
+    private final MemberService memberService;
+    private final SocketMessageFactory socketMessageFactory;
+
+    public void message(ClientMessage message,
+        @Header("roomId") String roomId,
+        @Header("type") ClientMessageType type) {
+
+        Long memberId = 1L;
+        Member member = memberService.findOneById(memberId);
+        if (type.isChat() || type.isEnter() || type.isExit()) {
+            socketMessageFactory.updateMessage(roomId, type, message, member);
+        }
+        ChatSendRequest chatSendRequest = createChatSendRequest(message, roomId, type, memberId);
+        chattingService.send(chatSendRequest);
+    }
+
+    private ChatSendRequest createChatSendRequest(ClientMessage message, String roomId, ClientMessageType type, Long memberId) {
+        return ChatSendRequest.builder()
+            .roomId(roomId)
+            .type(type)
+            .message(message.getMessage())
+            .senderId(memberId)
+            .build();
+    }
+}
--- src/main/java/algo_arena/chat/enums/ClientMessageType.java
+++ src/main/java/algo_arena/chat/enums/ClientMessageType.java
@@ -2,6 +2,18 @@ package algo_arena.chat.enums;
 
 public enum ClientMessageType {
 
-    CHAT, ENTER, EXIT, CREATE, CLOSE;
+    CHAT, ENTER, EXIT, CREATE, CLOSE
+    ;
 
+    public boolean isEnter() {
+        return this == ENTER;
+    }
+
+    public boolean isExit() {
+        return this == EXIT;
+    }
+
+    public boolean isChat() {
+        return this == CHAT;
+    }
 }
