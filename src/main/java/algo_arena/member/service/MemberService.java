0c0a12758e225d48ff8345491d5a6f3fa065d33c
silxbro <silbroeh@gmail.com>
2025-01-06 23:40:26 +0900

    feat: 회원(Member) 서비스 인증코드 메일 발송 및 검증 기능 추가
    - 인증코드 생성, 만료시간, 메일 제목 및 내용 주입
    - 메일 전송 및 인증코드 관련 예외 처리

--- src/main/java/algo_arena/member/service/MemberService.java
+++ src/main/java/algo_arena/member/service/MemberService.java
@@ -2,10 +2,16 @@ package algo_arena.member.service;
 
 import algo_arena.member.entity.Member;
 import algo_arena.member.repository.MemberRepository;
+import algo_arena.utils.auth.service.CodeAuthService;
 import algo_arena.utils.jwt.service.JwtTokenUtil;
 import algo_arena.utils.jwt.service.JwtUserDetailsService;
+import algo_arena.utils.mail.MailService;
+import jakarta.mail.MessagingException;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
+import java.util.Random;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -20,6 +26,11 @@ public class MemberService {
     private final JwtTokenUtil jwtTokenUtil;
     private final JwtUserDetailsService jwtUserDetailsService;
     private final PasswordEncoder passwordEncoder;
+    private final MailService mailService;
+    private final CodeAuthService codeAuthService;
+
+    @Value("${spring.mail.auth-code-expiration-millis}")
+    private long authCodeExpirationMillis;
 
     @Transactional
     public Member register(Member member) {
@@ -48,9 +59,36 @@ public class MemberService {
         final Member member = findMemberByEmail(email);
         UserDetails userDetails = jwtUserDetailsService.loadUserByUsername(member.getNickname());
         if (!passwordEncoder.matches(password, member.getPassword())) {
-            throw new IllegalStateException();
+            throw new IllegalStateException("패스워드가 일치하지 않습니다.");
         }
         return jwtTokenUtil.generateToken(userDetails);
+    }
+
+    @Transactional
+    public void sendAuthEmail(String email) {
+        String authCode = generateAuthCode();
+        codeAuthService.setAuthCodeAndExpiration(email, authCode, authCodeExpirationMillis);
+        String emailTitle = "[Algo-Arena] 이메일 인증코드 발송 메일입니다.";
+        String emailContent = "인증 코드: " + authCode;
+        try {
+            mailService.sendEmail(email, emailTitle, emailContent);
+        } catch (MessagingException e) {
+
+        } catch (UnsupportedEncodingException e) {
+
+        }
+    }
+
+    @Transactional
+    public void verifyAuthCode(String email, String enteredCode) {
+        String code = codeAuthService.getAuthCode(email);
+        if (code == null) {
+            throw new IllegalStateException("이메일 인증을 먼저 시도해 주세요.");
+        }
+        if (!code.equals(enteredCode)) {
+            throw new IllegalStateException("이메일 인증코드가 일치하지 않습니다.");
+        }
+        codeAuthService.markAuthAsCompleted(email);
     }
 
     @Transactional
@@ -71,5 +109,11 @@ public class MemberService {
     @Transactional
     public void delete(Long id) {
         memberRepository.deleteById(id);
+    }
+
+    private String generateAuthCode() {
+        Random random = new Random();
+        int authCode = 100000 + random.nextInt(900000);  // 100000~999999 사이의 숫자
+        return String.valueOf(authCode);
     }
 }
