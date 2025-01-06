03caabc33b1c3a00f97e5d2abedfdb5f71727499
silxbro <silbroeh@gmail.com>
2025-01-06 23:45:35 +0900

    feat: 이메일 인증코드 전송 및 검증 경로 무인증 접근가능 설정

--- src/main/java/algo_arena/config/security/WebSecurityConfig.java
+++ src/main/java/algo_arena/config/security/WebSecurityConfig.java
@@ -46,7 +46,7 @@ public class WebSecurityConfig {
             .cors(cors -> cors.configurationSource(corsConfigurationSource())) //CORS 설정 적용
             .csrf(AbstractHttpConfigurer::disable) //CSRF 보호 비활성화
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/login", "/register").permitAll() //인증 없이 접근 허용
+                .requestMatchers("/login", "/register", "/email-auth/**").permitAll() //인증 없이 접근 허용
                 .anyRequest().authenticated() //기타 요청은 인증 필요
             )
             .exceptionHandling(exceptionConfigurer -> exceptionConfigurer
