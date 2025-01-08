272bce13d6043fbbd0da484c31b2c6e2ad15bb2a
silxbro <silbroeh@gmail.com>
2025-01-08 12:25:30 +0900

    feat: Websocket 통신 interceptor 구현
    - WebSocket 연결 전 인증 처리: JWT 유효성 검사

--- src/main/java/algo_arena/config/socket/WebSocketConfig.java
+++ src/main/java/algo_arena/config/socket/WebSocketConfig.java
@@ -1,7 +1,9 @@
 package algo_arena.config.socket;
 
+import algo_arena.chat.handler.StompHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -12,10 +14,12 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    private final StompHandler stompHandler;
+
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/sub"); // 메시지를 브로드캐스팅할 경로
-        registry.setApplicationDestinationPrefixes("/pub"); // 클라이언트 요청 수신 경로
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/sub"); // 메시지를 브로드캐스팅할 경로
+        config.setApplicationDestinationPrefixes("/pub"); // 클라이언트 요청 수신 경로
     }
 
     @Override
@@ -23,5 +27,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/ws-stomp") // 클라이언트에서 연결할 엔드포인트
             .setAllowedOrigins("*") // CORS 허용
             .withSockJS(); // SockJS 사용 (브라우저 호환성)
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(stompHandler);
     }
 }
