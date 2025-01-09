e41709f6b7bc48569c5e74755ac64f2991ceb55e
silxbro <silbroeh@gmail.com>
2025-01-09 11:20:20 +0900

    feat: 채팅(Chat) service 변경내용 반영
    - Room 조회 로직 제거: controller에서 조회 후 매개변수 전달
    - receivers 조회 로직 제거
    - 채팅메시지 타입 변경 적용
    - sender 대신 sender name 정보 전달

--- src/main/java/algo_arena/chat/service/ChattingService.java
+++ src/main/java/algo_arena/chat/service/ChattingService.java
@@ -2,13 +2,9 @@ package algo_arena.chat.service;
 
 import algo_arena.chat.dto.response.ChatMessage;
 import algo_arena.chat.entity.ChatLog;
-import algo_arena.chat.enums.ClientMessageType;
+import algo_arena.chat.enums.MessageType;
 import algo_arena.chat.factory.ChatMessageFactory;
-import algo_arena.member.entity.Member;
 import algo_arena.room.entity.Room;
-import algo_arena.room.entity.RoomMember;
-import algo_arena.room.service.RoomFindService;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
@@ -21,34 +17,22 @@ public class ChattingService {
     private final ChannelTopic channelTopic;
     private final RedisTemplate<String, Object> redisTemplate;
     private final ChatMessageFactory chatMessageFactory;
-
-    private final RoomFindService roomFindService;
     private final ChatLogService chatLogService;
 
     /**
      * Room 에 message 발송
      */
-    public void send(String message, ClientMessageType type, String roomId, Member member) {
-        Room room = findRoom(roomId);
-        List<Member> receivers = findReceivers(room);
-        Long index = saveChatLog(type, message, member.getNickname(), roomId);
+    public void send(MessageType type, Room room, String senderName, String message) {
+        Long index = saveChatLog(type, room.getId(), senderName, message);
 
         ChatMessage chatMessage = chatMessageFactory.createMessage(
-            room.getId(), index, type, message, member, receivers);
+            type, room.getId(), index, senderName, message);
 
         sendToRedis(chatMessage);
     }
 
-    private Room findRoom(String roomId) {
-        return roomFindService.findRoomById(roomId);
-    }
-
-    private List<Member> findReceivers(Room room) {
-        return room.getRoomMembers().stream().map(RoomMember::getMember).toList();
-    }
-
-    private Long saveChatLog(ClientMessageType type, String message, String senderName, String roomId) {
-        ChatLog chatLog = ChatLog.create(type, message, senderName);
+    private Long saveChatLog(MessageType type, String roomId, String senderName, String message) {
+        ChatLog chatLog = ChatLog.create(type, senderName, message);
         return chatLogService.saveChatLog(roomId, chatLog);
     }
 
