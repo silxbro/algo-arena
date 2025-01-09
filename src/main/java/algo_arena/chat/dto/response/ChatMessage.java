806f03fe29bd05a4c9b6ca4e11022b9c5e58eade
silxbro <silbroeh@gmail.com>
2025-01-09 10:28:06 +0900

    feat: 채팅(Chat) 메시지 구조 변경
    - 타입 변경: SocketServerMessageType 실요성이 없다고 판단하여 삭제 예정
    - 필드 변경: 회원 객체 대신 닉네임 필드 지정
    - 필드 삭제: pub/sub 방식으로 메시지를 송수신할 때는 receiver 지정 불필요

--- src/main/java/algo_arena/chat/dto/response/ChatMessage.java
+++ src/main/java/algo_arena/chat/dto/response/ChatMessage.java
@@ -1,9 +1,7 @@
 package algo_arena.chat.dto.response;
 
-import algo_arena.chat.enums.SocketServerMessageType;
-import algo_arena.member.entity.Member;
+import algo_arena.chat.enums.ClientMessageType;
 import java.sql.Timestamp;
-import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -11,18 +9,12 @@ import lombok.Getter;
 @Builder
 public class ChatMessage {
 
+    private ClientMessageType type;
     private String roomId;
     private Long index;
-    private SocketServerMessageType type;
+    private String senderName;
     private String message;
-    private Member sender;
-    private List<Member> receivers;
 
     @Builder.Default
     private Timestamp sendTime = new Timestamp(System.currentTimeMillis());
-
-    public ChatMessage setType(SocketServerMessageType type) {
-        this.type = type;
-        return this;
-    }
 }
