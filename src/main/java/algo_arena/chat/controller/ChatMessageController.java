f68fe2e99e6327700cf5f9657fd52f3fceee9130
silxbro <silbroeh@gmail.com>
2025-01-09 11:18:58 +0900

    feat: 채팅(Chat) controller 변경사항 반영
    - 채팅 전송 요청 dto 변경
    - 채팅메시지 타입 변경
    - 채팅메시지 내용 셋팅을 위한 Room 정보 find
    - sender name 이외의 정보 불필요로 Member 정보 find 로직 삭제
    - StringMessageFactory를 적용한 메시지 내용 셋팅 적용

--- src/main/java/algo_arena/chat/controller/ChatMessageController.java
+++ src/main/java/algo_arena/chat/controller/ChatMessageController.java
@@ -1,17 +1,16 @@
 package algo_arena.chat.controller;
 
-import algo_arena.chat.dto.request.ClientMessage;
-import algo_arena.chat.enums.ClientMessageType;
-import algo_arena.chat.factory.SocketMessageFactory;
+import algo_arena.chat.dto.request.ChatSendRequest;
+import algo_arena.chat.enums.MessageType;
+import algo_arena.chat.factory.StringMessageFactory;
 import algo_arena.chat.service.ChattingService;
-import algo_arena.member.entity.Member;
-import algo_arena.member.service.MemberService;
+import algo_arena.room.entity.Room;
+import algo_arena.room.service.RoomFindService;
 import algo_arena.utils.jwt.service.JwtTokenUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
@@ -21,21 +20,20 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 public class ChatMessageController {
 
     private final ChattingService chattingService;
-    private final MemberService memberService;
-    private final SocketMessageFactory socketMessageFactory;
+    private final RoomFindService roomFindService;
+    private final StringMessageFactory stringMessageFactory;
     private final JwtTokenUtil jwtTokenUtil;
 
     @MessageMapping("/rooms/{roomId}/chat")
-    public void chat(@Payload ClientMessage message,
-        @DestinationVariable final String roomId,
-        @Header("type") ClientMessageType type,
+    public void chat(ChatSendRequest request,
+        @DestinationVariable("roomId") final String roomId,
+        @Header("type") MessageType type,
         @Header("token") String token) {
 
-        String username = jwtTokenUtil.extractUsername(token);
-        Member member = memberService.findMemberByNickname(username);
+        String senderName = jwtTokenUtil.extractUsername(token);
+        Room room = roomFindService.findRoomById(roomId);
+        String message = stringMessageFactory.createStringMessage(type, room, senderName, request.getMessage());
 
-        socketMessageFactory.insertMessage(message, type, roomId, member);
-
-        chattingService.send(message.getMessage(), type, roomId, member);
+        chattingService.send(type, room, senderName, message);
     }
 }
