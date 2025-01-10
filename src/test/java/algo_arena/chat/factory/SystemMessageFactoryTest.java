37cb8b8920174f6f88e32a3321b95522eebd6c8c
silxbro <silbroeh@gmail.com>
2025-01-10 11:34:21 +0900

    test: 알림성(시스템) 메시지 생성 기능 테스트

---
+++ src/test/java/algo_arena/chat/factory/SystemMessageFactoryTest.java
@@ -0,0 +1,91 @@
+package algo_arena.chat.factory;
+
+import static algo_arena.chat.enums.MessageType.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import algo_arena.member.entity.Member;
+import algo_arena.room.entity.Room;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemMessageFactoryTest {
+
+    @Autowired
+    SystemMessageFactory systemMessageFactory;
+
+    Room room;
+
+    String memberName;
+
+    @BeforeEach
+    void setUp() {
+        room = Mockito.mock(Room.class);
+        memberName = "Alice";
+    }
+
+    @Test
+    void create_EnterMessage() {
+        //given
+
+        //when
+        String result = systemMessageFactory.create(ENTER, room, memberName);
+
+        //then
+        assertThat(result).isEqualTo("[알림] Alice 님이 입장했습니다.");
+
+    }
+
+    @Test
+    void create_ExitMessage() {
+        //given
+
+        //when
+        String result = systemMessageFactory.create(EXIT, room, memberName);
+
+        //then
+        assertThat(result).isEqualTo("[알림] Alice 님이 퇴장했습니다.");
+    }
+
+    @Test
+    void create_CreateRoom() {
+        //given
+        Mockito.when(room.getName()).thenReturn("TestRoom");
+
+        //when
+        String result = systemMessageFactory.create(CREATE, room, memberName);
+
+        //then
+        assertThat(result).isEqualTo("[알림] Alice 님이 <TestRoom> 방을 생성했습니다.");
+    }
+
+    @Test
+    void create_ChangeHost() {
+        //given
+        Member newHost = Mockito.mock(Member.class);
+        Mockito.when(room.getHost()).thenReturn(newHost);
+        Mockito.when(newHost.getNickname()).thenReturn("John");
+
+        //when
+        String result = systemMessageFactory.create(CHANGE_HOST, room, null);
+
+        //then
+        assertThat(result).isEqualTo("[알림] 방장이 John 님으로 변경되었습니다.");
+    }
+
+    @Test
+    void create_InvalidMessageType() {
+        //given
+
+        //when
+
+        //then
+        assertThrows(NullPointerException.class, () ->
+            systemMessageFactory.create(null, room, memberName)
+        );
+    }
+}
