b3015eb9ffc47cb32fabb36347728f0ec863d9ff
silxbro <silbroeh@gmail.com>
2025-01-10 10:36:35 +0900

    refactor: 채팅 메시지 생성 - 알림(시스템) 메시지 한정하도록 리팩터링

--- src/main/java/algo_arena/chat/controller/ChatMessageController.java
+++ src/main/java/algo_arena/chat/controller/ChatMessageController.java
@@ -2,7 +2,7 @@ package algo_arena.chat.controller;
 
 import algo_arena.chat.dto.request.ChatSendRequest;
 import algo_arena.chat.enums.MessageType;
-import algo_arena.chat.factory.StringMessageFactory;
+import algo_arena.chat.factory.SystemMessageFactory;
 import algo_arena.chat.service.ChattingService;
 import algo_arena.room.entity.Room;
 import algo_arena.room.service.RoomFindService;
@@ -21,7 +21,7 @@ public class ChatMessageController {
 
     private final ChattingService chattingService;
     private final RoomFindService roomFindService;
-    private final StringMessageFactory stringMessageFactory;
+    private final SystemMessageFactory systemMessageFactory;
     private final JwtTokenUtil jwtTokenUtil;
 
     @MessageMapping("/rooms/{roomId}/chat")
@@ -30,10 +30,17 @@ public class ChatMessageController {
         @Header("type") MessageType type,
         @Header("token") String token) {
 
-        String senderName = jwtTokenUtil.extractUsername(token);
         Room room = roomFindService.findRoomById(roomId);
-        String message = stringMessageFactory.createStringMessage(type, room, senderName, request.getMessage());
+        String senderName = jwtTokenUtil.extractUsername(token);
+        String message = getMessageByType(type, room, senderName, request.getMessage());
 
         chattingService.send(type, room, senderName, message);
+    }
+
+    private String getMessageByType(MessageType type, Room room, String senderName, String message) {
+        if (type.isChatType()) {
+            return message;
+        }
+        return systemMessageFactory.create(type, room, senderName);
     }
 }
