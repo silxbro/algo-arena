ddd51e063bd24204060379802568c0793c8c5942
silxbro <silbroeh@gmail.com>
2025-01-14 18:53:01 +0900

    fix: 중복 Bean 등록으로 인한 Component 어노테이션 삭제

--- src/main/java/algo_arena/utils/audit/AuditorAwareImpl.java
+++ src/main/java/algo_arena/utils/audit/AuditorAwareImpl.java
@@ -5,9 +5,7 @@ import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.stereotype.Component;
 
-@Component
 public class AuditorAwareImpl implements AuditorAware<String> {
     @Override
     public Optional<String> getCurrentAuditor() {
