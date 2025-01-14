e3e4c20355efd034d36e7d45a49edb3e7d2a65a1
silxbro <silbroeh@gmail.com>
2025-01-15 00:14:13 +0900

    test: 회원(Member) 일반/인증 서비스 테스트

--- src/test/java/algo_arena/member/service/MemberServiceTest.java
+++ src/test/java/algo_arena/member/service/MemberServiceTest.java
@@ -1,9 +1,9 @@
 package algo_arena.member.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import algo_arena.member.entity.Member;
+import algo_arena.member.repository.MemberRepository;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -21,102 +21,67 @@ class MemberServiceTest {
     MemberService memberService;
 
     @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
     PasswordEncoder passwordEncoder;
 
     Member member;
 
+    String email = "test@example.com";
+    String nickname = "tester";
+    String password = "password";
+    String imgUrl = "https://my-image.com/origin-image.png";
+
     @BeforeEach
     void setUp() {
         member = Member.builder()
-            .email("bella@google.com")
-            .nickname("bella_Swan")
-            .password("bella!!!")
-            .imgUrl("https://my-image.com/bella.png")
+            .email(email)
+            .nickname(nickname)
+            .password(passwordEncoder.encode(password))
+            .imgUrl(imgUrl)
             .build();
-        memberService.register(member);
-    }
-
-    @Test
-    @DisplayName("회원 가입을 할 수 있다")
-    void register() {
-        //given
-        Member newMember = Member.builder()
-            .email("new@google.com")
-            .nickname("new")
-            .password("new!!!")
-            .imgUrl("https://my-image.com/new.png")
-            .build();
-
-        //when
-        Member registeredMember = memberService.register(newMember);
-
-        //then
-        assertThat(registeredMember).isEqualTo(newMember);
+        memberRepository.save(member);
     }
 
     @Test
     @DisplayName("닉네임으로 회원 검색 시, 대소문자를 구분하지 않고 해당 닉네임을 포함하는 모든 회원을 검색할 수 있다")
-    void findAllByNickname() {
+    void findMembersByNickname() {
         //given
+        String findNickname = "test";
 
         //when
-        List<Member> members = memberService.findMembersByNickname("swan");
+        List<Member> members = memberService.findMembersByNickname(findNickname);
 
         //then
         assertThat(members).containsExactly(member);
     }
 
     @Test
-    @DisplayName("로그인 정보가 올바를 때, 토큰이 발급된다")
-    void authenticate_Success() {
-        //given
-        String rightPassword = "bella!!!";
-
-        //when
-        String token = memberService.authenticate(member.getEmail(), rightPassword);
-
-        //then
-        assertThat(token).isNotNull();
-    }
-
-    @Test
-    @DisplayName("로그인 정보가 올바르지 않을 때, 예외가 발생한다")
-    void authenticate_Fail() {
-        //given
-        String wrongPassword = "wrong password";
-
-        //when
-
-        //then
-        assertThatThrownBy(() -> memberService.authenticate(member.getEmail(), wrongPassword))
-            .isInstanceOf(IllegalStateException.class);
-    }
-
-    @Test
-    @DisplayName("회원의 이미지를 변경할 수 있다")
+    @DisplayName("회원의 프로필 이미지를 변경할 수 있다")
     void updateImage() {
         //given
-        String imageUrl = "https://update-image.com/bella.png";
-        memberService.updateImage(member.getId(), imageUrl);
+        String newImgUrl = "https://my-image.com/new-image.png";
 
         //when
-        Member findMember = memberService.findMemberById(member.getId());
+        memberService.updateImage(member.getId(), newImgUrl);
+        Member updatedMember = memberService.findMemberById(member.getId());
 
         //then
-        assertThat(findMember.getImgUrl()).isEqualTo(imageUrl);
+        assertThat(updatedMember.getImgUrl()).isEqualTo(newImgUrl);
     }
 
     @Test
     @DisplayName("회원의 비밀번호를 변경할 수 있다")
     void changePassword() {
         //given
-        String password = "!!!bella";
-        memberService.changePassword(member.getId(), password);
+        String newPassword = "newPassword";
 
         //when
-        Member findMember = memberService.findMemberById(member.getId());
+        memberService.changePassword(member.getId(), newPassword);
+        Member updatedMember = memberService.findMemberById(member.getId());
 
         //then
-        assertThat(passwordEncoder.matches(password, findMember.getPassword())).isTrue();
+        assertThat(passwordEncoder.matches(newPassword, updatedMember.getPassword())).isTrue();
     }
 }
