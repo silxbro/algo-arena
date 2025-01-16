4c0795e6d4ca71d71c1d111ce9f901efc9fcf267
silxbro <silbroeh@gmail.com>
2025-01-16 11:28:19 +0900

    feat: 회원(Member) controller 변경
    - 프로필 이미지 변경 controller 삭제
    - 회원 정보 조회 세분화: 성적 및 프로필 조회

--- src/main/java/algo_arena/member/controller/MemberApiController.java
+++ src/main/java/algo_arena/member/controller/MemberApiController.java
@@ -1,16 +1,17 @@
 package algo_arena.member.controller;
 
-import algo_arena.member.dto.response.MemberDetailResponse;
 import algo_arena.member.dto.response.MemberListResponse;
-import algo_arena.member.dto.response.MemberUpdateResponse;
+import algo_arena.member.dto.response.MemberPerformanceResponse;
+import algo_arena.member.dto.response.MemberProfileResponse;
 import algo_arena.member.entity.Member;
 import algo_arena.member.service.MemberService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -37,36 +38,36 @@ public class MemberApiController {
     }
 
     /**
-     * 회원 정보 조회 (프로필)
+     * 회원 정보 조회 By Nickname (타인 조회용)
      */
-    @GetMapping("/{id}")
-    public ResponseEntity<MemberDetailResponse> findMember(@PathVariable("id") Long id) {
+    @GetMapping("/{nickname}")
+    public ResponseEntity<MemberPerformanceResponse> findMember(@PathVariable("nickname") String nickname) {
 
-        Member member = memberService.findMemberById(id);
+        Member member = memberService.findMemberByNickname(nickname);
 
-        return ResponseEntity.ok(MemberDetailResponse.from(member));
+        return ResponseEntity.ok(MemberPerformanceResponse.from(member));
     }
 
     /**
-     * 회원 프로필 이미지 변경
+     * 회원 정보 조회 By MyProfile (본인 조회용)
      */
-    @PatchMapping("/{id}/image")
-    public ResponseEntity<MemberUpdateResponse> updateImage(
-            @PathVariable("id") Long id,
-            @RequestBody String imgUrl) {
-        Member updatedMember = memberService.updateImage(id, imgUrl);
-        return ResponseEntity.ok(MemberUpdateResponse.from(updatedMember));
+    @GetMapping("/profile")
+    public ResponseEntity<MemberProfileResponse> myProfile(@AuthenticationPrincipal UserDetails userDetails) {
+
+        Member member = memberService.findMemberByNickname(userDetails.getUsername());
+
+        return ResponseEntity.ok(MemberProfileResponse.from(member));
     }
 
     /**
      * 회원 비밀번호 변경
      */
-    @PostMapping("/{id}/password")
-    public ResponseEntity<MemberUpdateResponse> changePassword(
-        @PathVariable("id") Long id,
+    @PostMapping("/{nickname}/password")
+    public ResponseEntity<Void> changePassword(
+        @PathVariable("nickname") String nickname,
         @RequestBody String password) {
-        Member updatedMember = memberService.changePassword(id, password);
-        return ResponseEntity.ok(MemberUpdateResponse.from(updatedMember));
+        Member updatedMember = memberService.changePassword(nickname, password);
+        return ResponseEntity.ok().build();
     }
 
     /**
