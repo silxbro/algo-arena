c532fb89c40588f16c0f4a22497a8368d11a5b3d
silxbro <silbroeh@gmail.com>
2025-01-16 11:21:33 +0900

    feat: 회원(Member) entity 구성 변경
    - 프로필 이미지 필드(imgUrl) 삭제
    - 퍼포먼스 관련 상세 항목 필드 추가

--- src/main/java/algo_arena/member/dto/request/MemberRegisterRequest.java
+++ src/main/java/algo_arena/member/dto/request/MemberRegisterRequest.java
@@ -1,7 +1,6 @@
 package algo_arena.member.dto.request;
 
 import algo_arena.member.entity.Member;
-import algo_arena.member.enums.Role;
 import lombok.Getter;
 
 @Getter
@@ -10,15 +9,12 @@ public class MemberRegisterRequest {
     private String email;
     private String password;
     private String nickname;
-    private String imgUrl;
 
     public Member toEntity() {
         return Member.builder()
             .email(email)
             .password(password)
             .nickname(nickname)
-            .imgUrl(imgUrl == null ? "default" : imgUrl)
-            .role(Role.USER)
             .build();
     }
 }
