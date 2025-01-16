b0ac4d6cb26abd860a96a1969862f696ff62dc1a
silxbro <silbroeh@gmail.com>
2025-01-16 17:46:43 +0900

    fix: 익명인증 사용자 Empty Auditor 반환 설정

--- src/main/java/algo_arena/utils/audit/AuditorAwareImpl.java
+++ src/main/java/algo_arena/utils/audit/AuditorAwareImpl.java
@@ -2,22 +2,27 @@ package algo_arena.utils.audit;
 
 import java.util.Optional;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public class AuditorAwareImpl implements AuditorAware<String> {
     @Override
     public Optional<String> getCurrentAuditor() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (isAuthenticated(authentication)) {
-            User user = (User) authentication.getPrincipal();
-            return Optional.of(user.getUsername());
+        if (!isAuthenticated(authentication) || isAnonymous(authentication)) {
+            return Optional.empty();
         }
-        return Optional.empty();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        return Optional.of(userDetails.getUsername());
     }
 
     private boolean isAuthenticated(Authentication authentication) {
         return authentication != null && authentication.isAuthenticated();
+    }
+
+    private boolean isAnonymous(Authentication authentication) {
+        return authentication instanceof AnonymousAuthenticationToken;
     }
 }
