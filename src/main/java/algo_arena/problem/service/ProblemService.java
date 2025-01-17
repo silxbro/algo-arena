241cb51a70f7aee8aa1b722a5ef843c01209b755
silxbro <silbroeh@gmail.com>
2025-01-17 13:26:15 +0900

    refactor: 문제(Problem) 서비스 권한확인 공통로직 분리, 의존관계 변경
    - 의존관계 변경: MemberService -> MemberRepository

--- src/main/java/algo_arena/problem/service/ProblemService.java
+++ src/main/java/algo_arena/problem/service/ProblemService.java
@@ -1,7 +1,7 @@
 package algo_arena.problem.service;
 
 import algo_arena.member.entity.Member;
-import algo_arena.member.service.MemberService;
+import algo_arena.member.repository.MemberRepository;
 import algo_arena.problem.entity.Problem;
 import algo_arena.problem.repository.ProblemRepository;
 import java.util.List;
@@ -16,14 +16,11 @@ import org.springframework.util.StringUtils;
 public class ProblemService {
 
     private final ProblemRepository problemRepository;
-    private final MemberService memberService;
+    private final MemberRepository memberRepository;
 
     @Transactional
     public Problem registerProblem(Problem problem, String memberName) {
-        Member member = memberService.findMemberByName(memberName);
-        if (!member.isAdmin()) {
-            throw new RuntimeException("문제 등록 권한이 없습니다. 관리자에게 문의하세요.");
-        }
+        checkAuth(memberName);
         return problemRepository.save(problem);
     }
 
@@ -37,10 +34,8 @@ public class ProblemService {
 
     @Transactional
     public void updateProblem(Long number, String title, String link, String memberName) {
-        Member member = memberService.findMemberByName(memberName);
-        if (!member.isAdmin()) {
-            throw new RuntimeException("문제 수정 권한이 없습니다. 관리자에게 문의하세요.");
-        }
+        checkAuth(memberName);
+
         Problem problem = findProblemByNumber(number);
         if (StringUtils.hasText(title)) {
             problem.changeTitle(title);
@@ -52,10 +47,14 @@ public class ProblemService {
 
     @Transactional
     public void deleteProblem(Long number, String memberName) {
-        Member member = memberService.findMemberByName(memberName);
-        if (!member.isAdmin()) {
-            throw new RuntimeException("문제 삭제 권한이 없습니다. 관리자에게 문의하세요.");
-        }
+        checkAuth(memberName);
         problemRepository.deleteByNumber(number);
+    }
+
+    private void checkAuth(String memberName) {
+        Member member = memberRepository.findByName(memberName).orElseThrow();
+        if (!member.isAdmin()) {
+            throw new RuntimeException("권한이 없습니다. 관리자에게 문의하세요.");
+        }
     }
 }
