f3141d9b1864a0bec7f426830f7bd813494c62f1
silxbro <silbroeh@gmail.com>
2025-01-17 10:16:19 +0900

    feat: 문제(Problem) 검색 서비스 구현
    - 문제 번호 검색 기능 추가

--- src/main/java/algo_arena/problem/service/ProblemService.java
+++ src/main/java/algo_arena/problem/service/ProblemService.java
@@ -4,6 +4,7 @@ import algo_arena.member.entity.Member;
 import algo_arena.member.service.MemberService;
 import algo_arena.problem.entity.Problem;
 import algo_arena.problem.repository.ProblemRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,5 +24,13 @@ public class ProblemService {
             throw new RuntimeException("문제 등록 권한이 없습니다. 관리자에게 문의하세요.");
         }
         return problemRepository.save(problem);
+    }
+
+    public List<Problem> findProblemsBySearch(Long minNumber, Long maxNumber, String title) {
+        return problemRepository.findProblemsBySearch(minNumber, maxNumber, title);
+    }
+
+    public Problem findProblemByNumber(Long id) {
+        return problemRepository.findByNumber(id).orElseThrow();
     }
 }
