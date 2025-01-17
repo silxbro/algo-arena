1d6c925633d019803df8183a7c79584b14db1ce2
silxbro <silbroeh@gmail.com>
2025-01-17 10:12:17 +0900

    feat: 문제(Problem) 등록 서비스 구현
    - 권한 확인을 위한 관리자 여부 반환 로직 회원(Member) entity에 추가

---
+++ src/main/java/algo_arena/problem/service/ProblemService.java
@@ -0,0 +1,27 @@
+package algo_arena.problem.service;
+
+import algo_arena.member.entity.Member;
+import algo_arena.member.service.MemberService;
+import algo_arena.problem.entity.Problem;
+import algo_arena.problem.repository.ProblemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ProblemService {
+
+    private final ProblemRepository problemRepository;
+    private final MemberService memberService;
+
+    @Transactional
+    public Problem registerProblem(Problem problem, String memberName) {
+        Member member = memberService.findMemberByName(memberName);
+        if (!member.isAdmin()) {
+            throw new RuntimeException("문제 등록 권한이 없습니다. 관리자에게 문의하세요.");
+        }
+        return problemRepository.save(problem);
+    }
+}
