4cf88f14bbf4b7ba2a9fcc28f1d55e0c7ece31b6
silxbro <silbroeh@gmail.com>
2025-01-20 17:45:28 +0900

    feat: 테스트룸(Room) 내부현황 실시간 업데이트 controller 구현

--- src/main/java/algo_arena/room/enums/RoomEvent.java
+++ src/main/java/algo_arena/room/enums/RoomEvent.java
@@ -2,6 +2,21 @@ package algo_arena.room.enums;
 
 public enum RoomEvent {
 
-    ENTER, EXIT, FULL, CREATE, DELETE, UPDATE, CHANGE_HOST
+    ENTER, EXIT, FULL, CREATE, DELETE, UPDATE, CHANGE_HOST;
 
+    public boolean isEnter() {
+        return this == ENTER;
+    }
+
+    public boolean isExit() {
+        return this == EXIT;
+    }
+
+    public boolean isChangeHost() {
+        return this == CHANGE_HOST;
+    }
+
+    public boolean isUpdate() {
+        return this == UPDATE;
+    }
 }
