0a7fed44f502fbe5aab53871f79fc560506844dc
silxbro <silbroeh@gmail.com>
2025-02-04 17:53:47 +0900

    refactor: 불필요 repository 삭제

--- src/main/java/algo_arena/room/repository/RoomMemberRepository.java
+++
@@ -1,8 +0,0 @@
-package algo_arena.room.repository;
-
-import algo_arena.room.entity.RoomMember;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomMemberRepository extends JpaRepository<RoomMember, Long> {
-
-}
