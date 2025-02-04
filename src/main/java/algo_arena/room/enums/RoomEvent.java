8c19cdb5c6eca930e34b181f437ba24e618c5e48
silxbro <silbroeh@gmail.com>
2025-02-04 18:02:07 +0900

    refactor: 미사용 메서드 삭제

--- src/main/java/algo_arena/room/enums/RoomEvent.java
+++ src/main/java/algo_arena/room/enums/RoomEvent.java
@@ -4,19 +4,7 @@ public enum RoomEvent {
 
     ENTER, EXIT, FULL, CREATE, DELETE, UPDATE, CHANGE_HOST;
 
-    public boolean isEnter() {
-        return this == ENTER;
-    }
-
-    public boolean isExit() {
-        return this == EXIT;
-    }
-
     public boolean isChangeHost() {
         return this == CHANGE_HOST;
-    }
-
-    public boolean isUpdate() {
-        return this == UPDATE;
     }
 }
