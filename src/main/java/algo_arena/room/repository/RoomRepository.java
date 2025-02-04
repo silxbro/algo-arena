a332da933a03e007d9febf2586a099352a51e3fe
silxbro <silbroeh@gmail.com>
2025-02-04 16:22:51 +0900

    fix: 테스트룸(Room) 연관 entity 탐색을 위한 EntityGraph 설정
    - RoomDetailResponse dto 생성 시, 연관 entity 정보가 모두 필요
    - controller 범위, 즉 준영속 상태에서 지연로딩 시도 실패
    - 따라서, 트랜잭션 범위 내에서 연관 entity 를 모두 가져오도록 설정

--- src/main/java/algo_arena/room/repository/RoomRepository.java
+++ src/main/java/algo_arena/room/repository/RoomRepository.java
@@ -1,8 +1,13 @@
 package algo_arena.room.repository;
 
 import algo_arena.room.entity.Room;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoomRepository extends JpaRepository<Room, String>, RoomQueryRepository {
+
+    @EntityGraph(attributePaths = {"host", "roomMembers", "roomProblems"})
+    Optional<Room> findById(String id);
 
 }
