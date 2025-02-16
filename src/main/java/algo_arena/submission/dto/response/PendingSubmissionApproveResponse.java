6792fd753997ef3a7df732e7b5ce3e01b8690abb
silxbro <silbroeh@gmail.com>
2025-02-17 00:03:17 +0900

    feat: 제출요청(PendingSubmission) 관련 응답 dto 생성
    - 제출(생성), 승인, 조회 관련 응답

---
+++ src/main/java/algo_arena/submission/dto/response/PendingSubmissionApproveResponse.java
@@ -0,0 +1,23 @@
+package algo_arena.submission.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class PendingSubmissionApproveResponse {
+
+    private String roomId;
+    private Long problemNumber;
+    private String memberName;
+    private Long index;
+
+    public static PendingSubmissionApproveResponse from(String roomId, Long problemNumber, String memberName, Long index) {
+        return PendingSubmissionApproveResponse.builder()
+            .roomId(roomId)
+            .problemNumber(problemNumber)
+            .memberName(memberName)
+            .index(index)
+            .build();
+    }
+}
