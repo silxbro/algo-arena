c3069cd2c62a7ee49cd5915c64b29c84b1367c51
silxbro <silbroeh@gmail.com>
2025-02-16 23:05:18 +0900

    feat: 제출요청(PendingSubmission) entity 생성
    결과를 증빙할 백준사이트 링크 첨부 및 승인여부 필드 추가

---
+++ src/main/java/algo_arena/submission/entity/PendingSubmission.java
@@ -0,0 +1,40 @@
+package algo_arena.submission.entity;
+
+import algo_arena.common.entity.BaseEntity;
+import jakarta.persistence.Lob;
+import java.io.Serializable;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+@Getter
+@Builder
+@RedisHash("submission")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
+public class PendingSubmission extends BaseEntity implements Serializable {
+
+    @Id
+    @Builder.Default
+    @EqualsAndHashCode.Include
+    private String id = UUID.randomUUID().toString();
+
+    private Long problemNumber;
+    private String memberName;
+    private String languageName;
+    private String resultDescription;
+    private String resultLink;
+
+    @Lob
+    private String solutionCode;
+
+    @Builder.Default
+    private Boolean isApproved = false;
+}
