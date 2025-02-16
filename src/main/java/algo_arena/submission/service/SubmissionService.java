ab4faf2ef3687e5515a23d359858c306d7fbd772
silxbro <silbroeh@gmail.com>
2025-02-16 23:39:54 +0900

    feat: 제출완료(Submission) 검색 및 상세 조회 기능 구현
    - 테스트방 실시간 조회가 아닌, 완료된 제출에 대한 조회 기능
    - 상세 조회의 경우, 해당 문제에 대해 정답 이력이 있는 사용자만 조회 가능하도록 검증로직 추가

--- src/main/java/algo_arena/submission/service/SubmissionService.java
+++ src/main/java/algo_arena/submission/service/SubmissionService.java
@@ -1,7 +1,14 @@
 package algo_arena.submission.service;
 
+import static algo_arena.submission.enums.SubmissionResult.CORRECT;
+
+import algo_arena.member.entity.Member;
+import algo_arena.member.service.MemberService;
+import algo_arena.submission.dto.request.SubmissionSearchRequest;
 import algo_arena.submission.entity.PendingSubmission;
+import algo_arena.submission.entity.Submission;
 import algo_arena.submission.repository.PendingSubmissionRedisRepository;
+import algo_arena.submission.repository.SubmissionRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,7 +19,9 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class SubmissionService {
 
+    private final SubmissionRepository submissionRepository;
     private final PendingSubmissionRedisRepository pendingSubmissionRepository;
+    private final MemberService memberService;
 
     @Transactional
     public Long submitSolution(String roomId, PendingSubmission pendingSubmission) {
@@ -33,5 +42,20 @@ public class SubmissionService {
             throw new RuntimeException();
         }
         return pendingSubmissionRepository.findOne(roomId, problemNumber, memberName).orElseThrow();
+    }
+
+    public List<Submission> findSubmissionsBySearch(SubmissionSearchRequest request) {
+        return submissionRepository.findSubmissions(request);
+    }
+
+    public Submission findSubmission(Long id, String memberName) {
+        Submission submission = submissionRepository.findById(id).orElseThrow();
+
+        Member member = memberService.findMemberByName(memberName);
+        if (!submissionRepository.existSubmission(submission.getProblem(), member, CORRECT)) {
+            throw new RuntimeException();
+        }
+
+        return submission;
     }
 }
