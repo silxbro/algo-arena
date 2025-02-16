fbc9801b412536efc245ff46a38bd2542d1c1bfb
silxbro <silbroeh@gmail.com>
2025-02-17 00:03:49 +0900

    feat: 제출(Submission) 관련 실시간 기능(테스트룸) controller 구현

---
+++ src/main/java/algo_arena/submission/controller/SubmissionSocketController.java
@@ -0,0 +1,79 @@
+package algo_arena.submission.controller;
+
+import algo_arena.submission.dto.request.PendingSubmissionApproveRequest;
+import algo_arena.submission.dto.request.PendingSubmissionFindRequest;
+import algo_arena.submission.dto.request.PendingSubmissionCreateRequest;
+import algo_arena.submission.dto.response.PendingSubmissionApproveResponse;
+import algo_arena.submission.dto.response.PendingSubmissionCreateResponse;
+import algo_arena.submission.dto.response.PendingSubmissionDetailResponse;
+import algo_arena.submission.entity.PendingSubmission;
+import algo_arena.submission.service.SubmissionService;
+import algo_arena.utils.jwt.service.JwtTokenUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class SubmissionSocketController {
+
+    private final SubmissionService submissionService;
+    private final JwtTokenUtil jwtTokenUtil;
+
+    /**
+     * 문제풀이 코드 제출
+     */
+    @MessageMapping("/rooms/{roomId}/submit")
+    @SendTo("/sub/rooms/{roomId}")
+    public PendingSubmissionCreateResponse submitSolution(@DestinationVariable("roomId") String roomId, @Header("token") String token,
+        @RequestBody PendingSubmissionCreateRequest request) {
+
+        String memberName = jwtTokenUtil.extractUsername(token);
+        Long problemNumber = request.getProblemNumber();
+        PendingSubmission pendingSubmission = PendingSubmission.create(problemNumber, memberName,
+            request.getLanguageName(), request.getResultDescription(), request.getResultLink(), request.getSolutionCode());
+
+        Long index = submissionService.submitSolution(roomId, pendingSubmission);
+        return PendingSubmissionCreateResponse.from(roomId, problemNumber, memberName, index);
+    }
+
+    /**
+     * 문제풀이 코드 상세 조회
+     */
+    @MessageMapping("/rooms/{roomId}/submission")
+    @SendTo("/sub/rooms/{roomId}")
+    public PendingSubmissionDetailResponse findPendingSubmission(@DestinationVariable("roomId") String roomId, @Header("token") String token,
+        @RequestBody PendingSubmissionFindRequest request) {
+
+        Long problemNumber = request.getProblemNumber();
+        String memberName = request.getMemberName();
+        String requestMemberName = jwtTokenUtil.extractUsername(token);
+
+        PendingSubmission pendingSubmission = submissionService.findPendingSubmission(roomId, problemNumber, memberName, requestMemberName);
+
+        return PendingSubmissionDetailResponse.from(roomId, pendingSubmission);
+    }
+
+    /**
+     * 문제풀이 코드 승인
+     */
+    @MessageMapping("/rooms/{roomId}/submission/approve")
+    @SendTo("/sub/rooms/{roomId}")
+    public PendingSubmissionApproveResponse approveSubmission(@DestinationVariable("roomId") String roomId, @Header("token") String token,
+        @RequestBody PendingSubmissionApproveRequest request) {
+
+        Long problemNumber = request.getProblemNumber();
+        String memberName = request.getMemberName();
+        String requestMemberName = jwtTokenUtil.extractUsername(token);
+
+        Long index = submissionService.approveSubmission(roomId, problemNumber, memberName, requestMemberName);
+
+        return PendingSubmissionApproveResponse.from(roomId, problemNumber, memberName, index);
+    }
+}
