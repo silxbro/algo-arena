9188b130d0663826dc7f5c02da4f108e3e0bb42c
silxbro <silbroeh@gmail.com>
2025-04-01 18:42:30 +0900

    feat: 최상위 예외 클래스 설계 변경
    - 일반 클래스 변경
    - 오류 타입별 고정 메시지 사용 가능(메시지 입력 필수 X)

--- src/main/java/algo_arena/common/exception/BaseException.java
+++ src/main/java/algo_arena/common/exception/BaseException.java
@@ -1,20 +1,30 @@
 package algo_arena.common.exception;
 
-import algo_arena.common.exception.enums.ErrorStatus;
+import algo_arena.common.exception.enums.ErrorType;
 import lombok.Getter;
 
 @Getter
-public abstract class BaseException extends RuntimeException {
+public class BaseException extends RuntimeException {
 
-    private final ErrorStatus errorStatus;
+    private final ErrorType errorType;
 
-    public BaseException(ErrorStatus errorStatus, String message) {
-        super(message);
-        this.errorStatus = errorStatus;
+    public BaseException(ErrorType errorType) {
+        super(errorType.getMessage());
+        this.errorType = errorType;
     }
 
-    public BaseException(ErrorStatus errorStatus, String message, Throwable cause) {
-        super(message, cause);
-        this.errorStatus = errorStatus;
+    public BaseException(ErrorType errorType, String errorMessage) {
+        super(errorMessage);
+        this.errorType = errorType;
+    }
+
+    public BaseException(ErrorType errorType, Throwable cause) {
+        super(errorType.getMessage(), cause);
+        this.errorType = errorType;
+    }
+
+    public BaseException(ErrorType errorType, String errorMessage, Throwable cause) {
+        super(errorMessage, cause);
+        this.errorType = errorType;
     }
 }
