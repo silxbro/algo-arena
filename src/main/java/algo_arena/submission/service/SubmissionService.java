99bdd7963941a8356607a49c04510ff2625a570f
silxbro <silbroeh@gmail.com>
2025-04-02 19:11:08 +0900

    feat: 제출관련 예외처리 구현

--- src/main/java/algo_arena/submission/service/SubmissionService.java
+++ src/main/java/algo_arena/submission/service/SubmissionService.java
@@ -1,14 +1,17 @@
 package algo_arena.submission.service;
 
+import static algo_arena.common.exception.enums.ErrorType.*;
 import static algo_arena.submission.enums.SubmissionResult.CORRECT;
 
 import algo_arena.member.entity.Member;
 import algo_arena.member.service.MemberService;
 import algo_arena.problem.entity.Problem;
 import algo_arena.problem.service.ProblemService;
+import algo_arena.room.service.RoomFindService;
 import algo_arena.submission.dto.request.SubmissionSearchRequest;
 import algo_arena.submission.entity.PendingSubmission;
 import algo_arena.submission.entity.Submission;
+import algo_arena.submission.exception.SubmissionException;
 import algo_arena.submission.repository.PendingSubmissionRedisRepository;
 import algo_arena.submission.repository.SubmissionRepository;
 import java.util.List;
@@ -23,6 +26,7 @@ public class SubmissionService {
 
     private final SubmissionRepository submissionRepository;
     private final PendingSubmissionRedisRepository pendingSubmissionRepository;
+    private final RoomFindService roomFindService;
     private final MemberService memberService;
     private final ProblemService problemService;
 
@@ -30,8 +34,11 @@ public class SubmissionService {
     public Long submitSolution(String roomId, PendingSubmission pendingSubmission) {
         Long problemNumber = pendingSubmission.getProblemNumber();
         String memberName = pendingSubmission.getMemberName();
+
+        validatePendingSubmission(roomId, problemNumber, memberName);
+
         if (pendingSubmissionRepository.hasSubmitted(roomId, problemNumber, memberName)) {
-            throw new RuntimeException();
+            throw new SubmissionException(SUBMISSION_DUPLICATED);
         }
         return pendingSubmissionRepository.save(roomId, pendingSubmission);
     }
@@ -44,21 +51,27 @@ public class SubmissionService {
         Problem problem = problemService.findProblemByNumber(problemNumber);
         Long index = submissionRepository.findLastIndex(member, problem).orElse(0L) + 1;
 
+        if (!pendingSubmissionRepository.hasSubmittedCorrectly(roomId, problemNumber, memberName)) { //승인 자격 검사
+            throw new SubmissionException(SUBMISSION_APPROVAL_NOT_ALLOWED);
+        }
         Submission submission = pendingSubmission.confirm(member, problem, index);
         submissionRepository.save(submission);
 
         return pendingSubmissionRepository.set(roomId, pendingSubmission);
     }
 
-    public List<PendingSubmission> findPendingSubmissions(String roomId, Long problemNumber) {
+    public List<PendingSubmission> findPendingSubmissions(String roomId, Long problemNumber, String requestMemberName) {
+        validatePendingSubmission(roomId, problemNumber, requestMemberName);
         return pendingSubmissionRepository.findAllByRoomProblem(roomId, problemNumber);
     }
 
     public PendingSubmission findPendingSubmission(String roomId, Long problemNumber, String memberName, String requestMemberName) {
+        validatePendingSubmission(roomId, problemNumber, requestMemberName);
         if (!pendingSubmissionRepository.hasSubmittedCorrectly(roomId, problemNumber, requestMemberName)) {
-            throw new RuntimeException();
+            throw new SubmissionException(SUBMISSION_REVIEW_NOT_ALLOWED);
         }
-        return pendingSubmissionRepository.findOne(roomId, problemNumber, memberName).orElseThrow();
+        return pendingSubmissionRepository.findOne(roomId, problemNumber, memberName)
+            .orElseThrow(() -> new SubmissionException(SUBMISSION_NOT_FOUND));
     }
 
     public List<Submission> findSubmissionsBySearch(SubmissionSearchRequest request) {
@@ -66,13 +79,24 @@ public class SubmissionService {
     }
 
     public Submission findSubmission(Long id, String memberName) {
-        Submission submission = submissionRepository.findById(id).orElseThrow();
+        if (id == null) {
+            throw new SubmissionException(NULL_VALUE);
+        }
+
+        Submission submission = submissionRepository.findById(id)
+            .orElseThrow(() -> new SubmissionException(SUBMISSION_NOT_FOUND));
 
         Member member = memberService.findMemberByName(memberName);
         if (!submissionRepository.existSubmission(submission.getProblem(), member, CORRECT)) {
-            throw new RuntimeException();
+            throw new SubmissionException(SUBMISSION_REVIEW_NOT_ALLOWED);
         }
 
         return submission;
+    }
+
+    private void validatePendingSubmission(String roomId, Long problemNumber, String memberName) {
+        memberService.findMemberByName(memberName);
+        roomFindService.findRoomById(roomId, memberName);
+        problemService.findProblemByNumber(problemNumber);
     }
 }
